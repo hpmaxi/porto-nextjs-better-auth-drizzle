b5622f0aeab6058050ecf805601df88ead544b65
Maximiliano Molina <hpmaxi@gmail.com>
2025-08-04 10:59:17 -0300

    cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server'
- 
-const allowedOrigins = ['http://localhost:3000', 'https://stg.id.porto.sh', "https://localhost:3000"]
- 
+import { clientEnv } from './env/clientEnv'
+
+const allowedOrigins = ['http://localhost:3000', 'https://stg.id.porto.sh', "https://localhost:3000", clientEnv.NEXT_PUBLIC_APP_URL]
+
 const corsOptions = {
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 }
- 
+
 export function middleware(request: NextRequest) {
   // Check the origin from the request
   const origin = request.headers.get('origin') ?? ''
   const isAllowedOrigin = allowedOrigins.includes(origin)
- 
+
   // Handle preflighted requests
   const isPreflight = request.method === 'OPTIONS'
- 
+
   if (isPreflight) {
     const preflightHeaders = {
       ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
@@ -22,21 +23,21 @@ export function middleware(request: NextRequest) {
     }
     return NextResponse.json({}, { headers: preflightHeaders })
   }
- 
+
   // Handle simple requests
   const response = NextResponse.next()
- 
+
   if (isAllowedOrigin) {
     response.headers.set('Access-Control-Allow-Origin', origin)
   }
- 
+
   Object.entries(corsOptions).forEach(([key, value]) => {
     response.headers.set(key, value)
   })
- 
+
   return response
 }
- 
+
 export const config = {
   matcher: '/api/:path*',
 }
